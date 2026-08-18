a7de87c47c1306d9da344d984c2cdc528e3a0ba6
Sarika Gite <sarikagite88@gmail.com>
2026-08-18 14:21:48 +0530

    Connect frontend to production backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hospital-management-backend-1-xu6z.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
